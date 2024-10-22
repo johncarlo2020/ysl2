3bafe9b4dd5cf466ec5ab7689c0d50a1456cea7f
whei2419 <87625823+whei2419@users.noreply.github.com>
2024-10-23 01:50:07 +0800

    update station list

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,13 @@
 <x-app-layout>
     <div class="dashboard main main-bg safari-padding">
         <div class="branding-container">@include('components.branding')</div>
-        <h1 class="station-born">BORN TO BE ICONIC</h1>
+        <h1 class="station-born">CELEBRATE IN GOLD</h1>
 
         <div class="content">
             @foreach ($stations as $station)
             @if($station->id != 5)
             <a class="title-container" href="{{ route('station.show', ['station' => $station->id]) }}">
-                <div class="tile {{ $station->id %2 == 0? '':'reverse' }}">
-                    <p class="station-number">{{$station->id}}</p>
+                <div class="tile">
                     <div class="img-container {{$station->status == true ? 'active':''}}">
                         <img src="{{ asset('images/S' . $station->id . '.jpg') }}" alt="" />
                         <div class="marker">
@@ -17,13 +16,9 @@
                     </div>
                     <div class="text-container-dashboard">
                         <p class="station-name-dashboard">
-                            @if($station->id == 1)
-                            LIBRE FRAGRANCE <br>& MAKE-UP DISCOVERY
+                            {{$station->id}}
 
-                            @else
                             {{ $station->name }}
-
-                            @endif
                         </p>
                     </div>
                 </div>
