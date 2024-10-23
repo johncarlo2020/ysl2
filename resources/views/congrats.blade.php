fd5d5669c17b9af1ad2291e5cf240188aa8eeeeb
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2024-10-23 17:06:13 +0800

    changes

--- resources/views/congrats.blade.php
+++ resources/views/congrats.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Wardah</title>
+    <title>YSL</title>
 
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
